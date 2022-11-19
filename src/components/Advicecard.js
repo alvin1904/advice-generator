11f9e3a64c538fb586158caa582d5a4d2145edd1
alvin1904 <alvin19official@gmail.com>
2022-11-19 07:57:08 +0530

    complete basic project

--- src/components/Advicecard.js
+++ src/components/Advicecard.js
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import dividerpc from '../images/pattern-divider-desktop.svg'
+import dividermobile from '../images/pattern-divider-mobile.svg'
+import diceimg from '../images/icon-dice.svg'
 
 function Advicecard(){
+    const [loading, setLoading]=useState(true)
+    const [advice, setAdvice]=useState({})
+    useEffect(()=>{
+        if(loading)
+          fetch('https://api.adviceslip.com/advice')
+              .then((response) => response.json())
+              .then((data) => setAdvice(data.slip))
+              .then(console.log("data fetched"))
+        setLoading(false)    
+    },[loading])
 
-    return <div className='advicecard'>
-        dfgsf
-    </div>
+    const handleRefresh = ()=>{
+        setLoading(true)
+    }
+
+    return <>
+        <div className='advicecard'>
+            <label className='adviceno'>advice #{advice.id}</label>
+            <div className='advice'>"{advice.advice}"</div>
+            <div className="divider">
+                <img src={dividerpc} className="dividerpc"></img>
+                <img src={dividermobile} className="dividermobile"></img>
+            </div>
+        </div>
+        <div className='dice' onClick={handleRefresh}>
+            <img src={diceimg} className="diceimg rotate"></img>
+        </div>
+    </>
+    
+
 }
 
 export default Advicecard;
